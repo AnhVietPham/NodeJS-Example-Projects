ef356482a630b2528a6351ceeeac822aa27e36d9
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2020-03-24 13:37:07 +0700

    Express router

--- express_router_rest_api/server.js
+++ express_router_rest_api/server.js
@@ -1,50 +1,49 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const dishRouter = require('./routes/dishRouter')
 
-app.use(bodyParser.json());
+// app.all('/dishes', (req, res, next) => {
+//     res.statusCode = 400;
+//     res.setHeader('Content-Type','text/plain');
+//     next();
+// });
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 400;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
+// app.get('/dishes', (req, res, next) => {
+//     res.end("We will send all users to You!!")
+// });
 
-app.get('/dishes', (req, res, next) => {
-    res.end("We will send all users to You!!")
-});
+// app.post('/dishes', (req, res, next) => {
+//     res.end(`Hey Guy! We will give you to know more about information of Dish. Name Dish : ${req.body.nameDish} and Description : ${req.body.descriptionDish}`)
+// });
 
-app.post('/dishes', (req, res, next) => {
-    res.end(`Hey Guy! We will give you to know more about information of Dish. Name Dish : ${req.body.nameDish} and Description : ${req.body.descriptionDish}`)
-});
+// app.put('/dishes', (req, res, next) => {
+//     res.statusCode = 300;
+//     res.end('PUT operation does not support on /dishes');
+// });
 
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 300;
-    res.end('PUT operation does not support on /dishes');
-});
+// app.delete('/dishes', (req, res, next) => {
+//     res.end('Delete all Dishes!!!')
+// });
 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Delete all Dishes!!!')
-});
+// app.get('/dishes/:dishId', (req, res, next) => {
+//     res.end(`Hey guy! Now we will send detail of is dish: ${req.params.dishId} to you!!`)
+// });
 
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end(`Hey guy! Now we will send detail of is dish: ${req.params.dishId} to you!!`)
-});
+// app.post('/dishes/:dishId', (req, res, next) => {
+//     res.statusCode = 403;
+//     res.end(`POST operator does not support on dishes/ : ${req.params.dishId}`);
+// })
 
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end(`POST operator does not support on dishes/ : ${req.params.dishId}`);
-})
+// app.put('/dishes/:dishId', (req, res, next) => {
+//     res.write(`Updating with dish: ${req.params.dishId} `);
+//     res.end(`We are updating your dish with dishId = ${req.params.dishId} and Content is updated nameDish : ${req.body.nameDish} and description : ${req.body.descriptionDish}`);
+// });
 
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write(`Updating with dish: ${req.params.dishId} `);
-    res.end(`We are updating your dish with dishId = ${req.params.dishId} and Content is updated nameDish : ${req.body.nameDish} and description : ${req.body.descriptionDish}`);
-});
+// app.delete('/dishes/:dishId', (req, res, next) => {
+//     res.end(`We are deleting your dish : ${req.params.dishId}`);
+// })
 
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end(`We are deleting your dish : ${req.params.dishId}`);
-})
+app.use('/dishes', dishRouter);
 
 app.listen(9999, () => {
     console.log('Server is connecting on port 9999!')
