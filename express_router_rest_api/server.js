f92126cc31feb0e85f256137dac159ba25ddf1fa
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2020-03-25 16:43:20 +0700

    done assignment using express

--- express_router_rest_api/server.js
+++ express_router_rest_api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const dishRouter = require('./routes/dishRouter')
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.listen(9999, () => {
     console.log('Server is connecting on port 9999!')
