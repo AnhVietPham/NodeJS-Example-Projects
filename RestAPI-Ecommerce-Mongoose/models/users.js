f1f5f2cbb74e47de73c80be3aaf8303642eb02b5
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-07-25 13:55:47 +0700

    hashing password of user with bcrypt

--- RestAPI-Ecommerce-Mongoose/models/users.js
+++ RestAPI-Ecommerce-Mongoose/models/users.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt =  require('bcrypt');
 const Schema = mongoose.Schema
 
-const User = new Schema({
+const UserSchema = new Schema({
        account:{
               type: String,
               default: ""
@@ -16,4 +17,17 @@ const User = new Schema({
        }
 })
 
-module.exports = mongoose.model('User', User);
+UserSchema.pre('save', async (next) => {
+       const user = this;
+       const hash = await bcrypt.hash(this.password, 10);
+       this.password = hash;
+       next();
+});
+
+UserSchema.methods.isValidPassword =  async (pass) => {
+       const user = this;
+       const compare = await bcrypt.compare(pass, user.password);
+       return compare;
+}
+
+module.exports = mongoose.model('User', UserSchema);
